98c5400ef963ff703a3e3777ae3b07723af36bb9
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-05-23 09:57:36 +0100

    add folder uml and img

--- src/main/java/com/constructionxpert/utils/DatabaseUtils.java
+++ src/main/java/com/constructionxpert/utils/DatabaseUtils.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseUtils {
     private static final String URL = "jdbc:mysql://localhost:3306/constructionxpert";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
